60c1310b4a7d9bf270a17137995bd14d59382c00
ShiroNoMercy <108806813+Xavieroffcial@users.noreply.github.com>
2023-03-19 08:49:46 +0800

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const chalk = require('chalk')
 // EDIT DISINI
-global.owner = ['6282127075205'] // no own
-global.author = 'KurrXd' // nama author 
-global.packname = 'xd bot whatsapp' // nama pack sticker
-global.namabot = 'XD BOTZ' // nama bot mu
-global.group = 'https://bit.ly/xdbotwhatsapp' // grup mu
-global.pic = 'https://telegra.ph/file/36211850fa46193ef96ad.jpg' // logo lu
+global.owner = ['6283862006229'] // no own
+global.author = 'FenrirXd' // nama author 
+global.packname = '©Miku Botz' // nama pack sticker
+global.namabot = 'MIKU BOTZ' // nama bot mu
+global.group = 'https://chat.whatsapp.com/KV5vkfzBKR65C8gw7xJ5yC' // grup mu
+global.pic = 'https://telegra.ph/file/f2da9981aae68c0f9113c.jpg' // logo lu
 
 
 let file = require.resolve(__filename)
